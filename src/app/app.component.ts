72fdf5bf886c7ce39930e6abb24d69dd2add2082
tanaphat jaroenponvijit <13441@sai.ac.th>
2024-09-03 12:19:06 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { NavController } from '@ionic/angular';
+import { AboutPage } from './about/about.page';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private a:
+    NavController
+  ) {}
+
+
+aboutPage() {
+  this.a.navigateForward
+  ('/about')
+
+}
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,17 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private t: ToastController) {}
+
+
+  async presentToast(position: 'top' | 'middle' | 'bottom') {
+    const toast = await this.t.create({
+      message: 'หวัดดีจั๊ปปป',
+      duration: 1500,
+      position: position,
+    });
+
+    await toast.present();
+  }
 
 }
